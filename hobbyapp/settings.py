5b636f0afb78bd907fdb91d9bca0d7ba9839b285
thomas-dunlop <dunlopt12@gmail.com>
2021-10-07 10:00:07 -0400

    Adding Heroku URL to allowed_hosts

--- hobbyapp/settings.py
+++ hobbyapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-y)qwjw4bzckuw^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hobby-project-tracker.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
